043703db10d62967016dd35c7cb9b92c9cb72d8f
kawarabata <kwrbt0404@gmail.com>
2023-03-21 16:13:42 +0900

    コンプリート率の表示を修正

--- lib/components/completion_rate_section.dart
+++ lib/components/completion_rate_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uchugo_collection/pages/history_page.dart';
 import 'package:uchugo_collection/providers/checked_kana_chars_provider.dart';
 
 class CompletionRateSection extends ConsumerWidget {
@@ -10,32 +11,54 @@ class CompletionRateSection extends ConsumerWidget {
     ref.watch(checkedKanaCharsProvider);
     final completionRate = ref.watch(checkedKanaCharsProvider.notifier).completionRate;
     final completionRateInPercent = ref.watch(checkedKanaCharsProvider.notifier).completionRateInPercent;
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text('コンプリート率'),
-            Text('$completionRateInPercent%'),
-          ],
-        ),
-        const SizedBox(height: 8),
-        TweenAnimationBuilder(
-          tween: Tween<double>(begin: 0, end: completionRate),
-          duration: const Duration(milliseconds: 250),
-          builder: (context, double value, _) {
-            return ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(2)),
-              child: LinearProgressIndicator(
-                backgroundColor: Colors.orange,
-                valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-                minHeight: 30,
-                value: value,
-              ),
-            );
-          },
-        ),
-      ],
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const HistoryPage()),
+        );
+      },
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('コンプリート率'),
+              Text('$completionRateInPercent%'),
+            ],
+          ),
+          const SizedBox(height: 8),
+          _buildCompletionRateBar(completionRate),
+          const SizedBox(height: 4),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('履歴を見る︎'),
+                Text('▶︎', style: TextStyle(fontSize: 12)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCompletionRateBar(double completionRate) {
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0, end: completionRate),
+      duration: const Duration(milliseconds: 250),
+      builder: (context, double value, _) {
+        return ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(2)),
+          child: LinearProgressIndicator(
+            backgroundColor: Colors.orange,
+            valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+            minHeight: 28,
+            value: value,
+          ),
+        );
+      },
     );
   }
 }
